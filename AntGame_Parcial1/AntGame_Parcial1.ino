79ce44c0233d88f63dbbcaa49a57dcefcec42ff4
JorgeAzzuFranco <00074216@uca.edu.sv>
2018-09-11 02:26:58 -0600

    Ant2D con validacion de ganador

--- AntGame_Parcial1/AntGame_Parcial1.ino
+++ AntGame_Parcial1/AntGame_Parcial1.ino
@@ -12,58 +12,68 @@ void setup() {
 
 //Variables
 const int dpi = 6; //densidad de pantalla
-int contador = 35; //LEDs restantes
 int posX;
 int posY;
+int contador = 0;
 int lectura = 0;
-int matriz[1][dpi][dpi] =
-{ //Matriz fisica
-  { //Columna
-    {1, 1, 1, 1, 1, 1}, //fila 0
-    {1, 1, 1, 1, 1, 1}, //fila 1
-    {1, 1, 1, 1, 1, 1}, //fila 2
-    {1, 1, 1, 1, 1, 1}, //fila 3
-    {1, 1, 1, 1, 1, 1}, //fila 4
-    {1, 1, 1, 1, 1, 1}  //fila 5
-  }
-};
-int matrix[1][dpi][dpi] =
-{ //Matriz virtual
-  { //Columna
-    {1, 1, 1, 1, 1, 1}, //fila 0
-    {1, 1, 1, 1, 1, 1}, //fila 1
-    {1, 1, 1, 1, 1, 1}, //fila 2
-    {1, 1, 1, 1, 1, 1}, //fila 3
-    {1, 1, 1, 1, 1, 1}, //fila 4
-    {1, 1, 1, 1, 1, 1}  //fila 5
-  }
+
+int matriz[dpi][dpi] =
+  //Matriz fisica
+{ //Columna
+  {1, 1, 1, 1, 1, 1}, //fila 0
+  {1, 1, 1, 1, 1, 1}, //fila 1
+  {1, 1, 1, 1, 1, 1}, //fila 2
+  {1, 1, 1, 1, 1, 1}, //fila 3
+  {1, 1, 1, 1, 1, 1}, //fila 4
+  {1, 1, 1, 1, 1, 1}  //fila 5
 };
 
+int matVirt[dpi][dpi] =
+  //Matriz virtual
+{ //Columna
+  {0, 0, 0, 0, 0, 0}, //fila 0
+  {0, 0, 0, 0, 0, 0}, //fila 1
+  {0, 0, 0, 0, 0, 0}, //fila 2
+  {0, 0, 0, 0, 0, 0}, //fila 3
+  {0, 0, 0, 0, 0, 0}, //fila 4
+  {0, 0, 0, 0, 0, 0}  //fila 5
+};
+
+int ganador[dpi][dpi] =
+  { //Columna
+    {0, 1, 1, 1, 1, 0}, //fila 0
+    {0, 1, 1, 1, 1, 0}, //fila 1
+    {0, 0, 1, 1, 0, 0}, //fila 2
+    {0, 0, 1, 1, 0, 0}, //fila 3
+    {0, 0, 1, 1, 0, 0}, //fila 4
+    {0, 1, 0, 0, 1, 0}  //fila 5
+  };
 
 void loop() {
 
   botonReinicio();
+  validarGanador();
 
   posX = map(analogRead(A1), 0, 1023, 0, 5);
   posY = map(analogRead(A0), 0, 1023, 0, 5);
 
-  if (matriz[0][posX][posY] == 1) {
-    matriz[0][posX][posY] = 0;
+  if (matriz[posX][posY] == 0) {
+    digitalWrite(posX, HIGH);
+    digitalWrite(posY + 6, LOW);
+  }
+
+  if (matriz[posX][posY] == 1) {
+    matriz[posX][posY] = 0;
   }
 
   printMatriz();
-
-
-
 }
 
 void limpiarMatriz() {
   //Llena la matriz de unos
-  for (int i = 0; i < 1; i++) {
-    for (int j = 0; j < dpi; j++) {
-      for (int k = 0; k < dpi; k++) {
-        matriz[i][j][k] = 1;
-      }
+  for (int j = 0; j < dpi; j++) {
+    for (int k = 0; k < dpi; k++) {
+      matriz[j][k] = 1;
     }
   }
 }
@@ -79,21 +89,49 @@ void botonReinicio() {
     lectura = !lectura;
   }
 
-  posX = 1;
-  posY = 1;
+}
+
+void validarGanador(){
+  contador = 0;
+  for (int i = 0; i < dpi; i++) { //Columnas
+      for (int j = 0; j < dpi; j++) { //Filas
+        if (matriz[i][j] == 0) {
+          contador++;
+        }
+      }
+  }
+
+  if (contador > 35) {
+    showGanador();
+  }
+}
+
+void showGanador(){
+  int tiempo = 0;
+  while(tiempo < 1000){
+    for (int i = 0; i < dpi; i++) { //Columnas
+      for (int j = 0; j < dpi; j++) { //Filas
+        if (ganador[i][j] == 1) {
+          digitalWrite(i, HIGH);
+          digitalWrite(j + 6, LOW);
+        }
+        limpiar();
+      }
+    }
+    tiempo++;
+  }
+  limpiarMatriz();
 }
 
 void printMatriz() {
-  for (int frame = 0; frame < 1; frame++) { //Frame
-    for (int repeticion = 0; repeticion < 50; repeticion++) {
-      for (int i = 0; i < dpi; i++) { //Columnas
-        for (int j = 0; j < dpi; j++) { //Filas
-          if (matriz[frame][i][j] == 1) {
-            digitalWrite(i, HIGH);
-            digitalWrite(j + 6, LOW);
-          }
-          limpiar();
+  for (int repeticion = 0; repeticion < 50; repeticion++) {
+    for (int i = 0; i < dpi; i++) { //Columnas
+      for (int j = 0; j < dpi; j++) { //Filas
+        if (matriz[i][j] == 1) {
+          digitalWrite(i, HIGH);
+          digitalWrite(j + 6, LOW);
         }
+        limpiar();
       }
     }
   }
